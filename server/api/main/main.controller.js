00e5165e958ae6b21bdc50605c544443155477bd
won <wjin0352@gmail.com>
2016-08-01 18:11:21 -0400

    working on simple node api with file structure

---
+++ server/api/main/main.controller.js
@@ -0,0 +1,7 @@
+function MainController() {}
+
+MainController.prototype.getRoot = function(req, res) {
+  res.sendFile('../client/index.html');
+}
+
+module.exports = MainController.prototype;
---
+++ server/config/variables.express.js
@@ -0,0 +1,8 @@
+module.exports ={
+  EXPORT_PORT: 5000,
+  EXPRESS_LISTEN_MESSAGE: 'Listening on port ',
+  MONGODB: {
+    PORT: '',
+    URI: ''
+  }
+};
